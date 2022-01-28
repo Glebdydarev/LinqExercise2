6ab3e1211b2d36258567d8d601320a1e549e4de5
Gleb Dudarev <glebdydarev@mail.ru>
2022-01-28 15:07:05 -0600

    done

---
+++ LinqExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        
+        {
+            
+            List<string> videogame = new List<string>() { "Grand Theft Auto", "GTA2", "GTA3","GTA4", "GTA5" };
+
+            var gamelist = videogame.OrderBy(name => name.Length);
+
+            foreach (var name in gamelist)
+            {
+                Console.WriteLine(name);
+            }
+
+           
+
+        }
+    }
+}
